bf45e700fccac7b643e9f57ffb43f60dd8f57af6
Hano <han.fu@mail.mcgill.ca>
2022-08-21 20:53:37 -0400

    Make notes fade out when unplayed

--- myscript.js
+++ myscript.js
@@ -10,16 +10,27 @@ function setAudio(color) {
     noteClasses.forEach((note) => {
         note.audio = new Audio(`notes/${color}/${notes[i]}.mp3`);
         note.addEventListener("mousedown", () => {
+            note.audio.pause()
+            note.audio.currentTime = 0;
+            note.audio.volume = 1;
             note.audio.play();
         });
         i++;
         ["mouseup", "mouseout"].forEach((evt) => {
             note.addEventListener(evt, () => {
-                note.audio.pause();
-                note.audio.currentTime = 0;
+                aud_fade(note, evt);        
             });
         });
     });
+}
+
+function aud_fade(note, evt) {
+    if (note.audio.volume < 1 & evt === "mouseout") console.log(note.audio.volume);
+    else if (Math.round(note.audio.volume * 10) > 0.2) {
+        console.log(note.audio.volume);
+        note.audio.volume -= 0.25;
+        setTimeout(aud_fade, 70, note);
+    }
 }
 
 setAudio("white");
