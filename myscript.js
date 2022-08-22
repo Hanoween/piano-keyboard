f4887da679ae103c23b529cb8b30e8dec707afa6
Hano <han.fu@mail.mcgill.ca>
2022-08-22 13:34:56 -0400

    Add reverb effect when pressing space key

--- myscript.js
+++ myscript.js
@@ -10,24 +10,50 @@ function setAudio(color) {
     noteClasses.forEach((note) => {
         note.audio = new Audio(`notes/${color}/${notes[i]}.mp3`);
         note.addEventListener("mousedown", () => {
+            note.isClicked = true;
             note.audio.pause()
             note.audio.currentTime = 0;
             note.audio.volume = 1;
             note.audio.play();
+            note.classList[0] === "black" ? note.style.backgroundColor = "#4e4e4f" : note.style.backgroundColor = "#c4c0b5";
         });
         i++;
+
         ["mouseup", "mouseout"].forEach((evt) => {
             note.addEventListener(evt, () => {
-                aud_fade(note, evt);        
+                note.isClicked = false;
+                note.style.backgroundColor = null;
+                aud_fade(note, evt);
             });
         });
+
     });
 }
 
-function aud_fade(note, evt) {
-    if (note.audio.volume < 1 & evt === "mouseout") console.log(note.audio.volume);
+let reverb = false;
+window.addEventListener("keypress", (event) => {
+    if (event.key === ' ' || event.key === 'Spacebar') {
+        reverb = true;
+    }
+});
+
+window.addEventListener("keyup", (event) => {
+    reverb = false;
+    if (event.key === ' ' || event.key === 'Spacebar') {
+        let allNotes = document.querySelectorAll(".black, .white");
+        allNotes.forEach((note) => {
+            if (!note.isClicked) {
+                aud_fade(note);
+            }
+        });
+    }
+});
+
+function aud_fade(note, evt = "mouseup") {
+    if (reverb || note.audio.volume < 1 & evt === "mouseout") {
+        return;
+    }
     else if (Math.round(note.audio.volume * 10) > 0.2) {
-        console.log(note.audio.volume);
         note.audio.volume -= 0.2;
         setTimeout(aud_fade, 20, note);
     }
